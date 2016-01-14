e317f58835e630712fa6a3ff9e22806ef1c7ec9a
Dragan Gaic <dragan.gaic@gmail.com>
2016-01-14 22:57:05 +0100

    Update

--- app/pages/services/MovieService.js
+++ app/pages/services/MovieService.js
@@ -1,5 +1,5 @@
-import {Inject} from 'angular2/core';
-import { Http, Headers } from 'angular2/http';
+import { Inject } from 'angular2/core';
+import { Http} from 'angular2/http';
 import 'rxjs/add/operator/map';
 
 export class MovieService {  
@@ -8,9 +8,8 @@ export class MovieService {
 		this.http = http
 	}	
 
-	searchMovies(movieName): Observable<> {
+	searchMovies(movieName) {
 		var url = 'http://api.themoviedb.org/3/search/movie?query=&query=' + encodeURI(movieName) + '&api_key=5fbddf6b517048e25bc3ac1bbeafb919';
-		this.response = this.http.get(url).map(res => res.json());
-	  return this.response;
+		return this.http.get(url).map(res => res.json());
 	}
 }
